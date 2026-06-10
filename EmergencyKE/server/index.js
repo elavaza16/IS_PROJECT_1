e9ec63ca19506c956e8ac9b85a1fe15f8bc29818
Elavaza2025 <sandra.elavaza@strathmore.edu>
2026-06-10 18:40:40 +0300

    rm

--- EmergencyKE/server/index.js
+++ EmergencyKE/server/index.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({origin: ['http://localhost:5173', process.env.CLIENT_URL,]}));
+app.use(cors({ origin: ['http://localhost:5173', process.env.CLIENT_URL] }));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth.routes'));
 
-app.get('/', (req, res) => res.json({ message: 'JioKoa API running' }));
+app.get('/', (req, res) => res.json({ message: 'EmergencyKE API running' }));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
